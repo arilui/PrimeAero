0cfa0ab415e4124fe52c387d0f9b67d57676b11d
arilui <plui0212@gmail.com>
2025-01-14 13:20:21 -0800

    Update Main.kt
    there was an update

--- PrimeAeroApp/src/Main.kt
+++ PrimeAeroApp/src/Main.kt
@@ -3,7 +3,9 @@
 class Person(val name: String, val age: Int)
 fun main() {
     val person = Person("Alice", 30)
+    val person2 = Person("Jake", 560)
 
     // Print the person's details
     println("Name: ${person.name}, Age: ${person.age}")
+    println("Name: ${person2.name}, Age: ${person2.age}")
 }
